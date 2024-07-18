d62901d9ddd51e6cc290101a4db6c55d42927bf5
Rodrigo <rodrigo.franciscom@gmail.com>
2024-07-18 13:03:08 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [color, setColor] = useState ("red")
+
+  const changeColor = (color) => { setColor(color)}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="palo"></div>
+      <div className="semaforoBody">
+        <div
+          className={`red ${color === "red" ? "brillo" : ""}`}
+          onClick={() => changeColor("red")}
+        ></div>
+        <div
+          className={`yellow ${color === "yellow" ? "brillo" : ""}`}
+          onClick={() => changeColor("yellow")}
+        ></div>
+        <div
+          className={`green ${color === "green" ? "brillo" : ""}`}
+          onClick={() => changeColor("green")}
+        ></div>
+      </div>
     </div>
   );
-}
+  
+  }
+
+
 
 export default App;
